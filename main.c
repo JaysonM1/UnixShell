0ed9c52accb4b8213657feceb9007a8c605fef15
JaysonM1 <jaysonm@udel.edu>
2022-10-06 16:41:50 -0400

    set up args

--- main.c
+++ main.c
@@ -20,6 +20,28 @@ int main(int argc, char **argv, char **envp){
     char *prompt = calloc(PROMPTMAX, sizeof(char));
     char *pwd;
     setenv("noclobber", "0", 1);
-    printf("Hello World\n");
+    
+    int uid, i, status, argsct, go = 1;
+    struct passwd *password_entry;
+    char *homedir;
+    struct pathelement *pathlist;
+
+    char *prev = getcwd(NULL,0);
+    char *save = getcwd(NULL,0);
+    int cdaint=1;
+
+
+
+    //command and parameter variables
+    char **args = calloc(MAXARGS, sizeof(char*));
+    //char *para[20];
+    char **para = calloc(MAXARGS, sizeof(char*));
+
+    //command and parameter variables
+    char **args = calloc(MAXARGS, sizeof(char*));
+    //char *para[20];
+    char **para = calloc(MAXARGS, sizeof(char*));
+
+    
     return 0;
 }
